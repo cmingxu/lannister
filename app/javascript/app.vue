22fc1a6c45bb5396cc9f12ac3cff84019232b730
xuchunming <cming.xu@gmail.com>
2019-05-17 14:04:01 +0800

    add mint-ui
    Signed-off-by: xuchunming <cming.xu@gmail.com>

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,51 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
-    <HelloComponent />
+    <Button size="large" type="danger">foobar</Button>
+    <Tabbar v-model="selected">
+    <TabBar id="tab1">
+    <img slot="icon" src="">
+    tab1
+    </TabBar>
+    <TabBar id="tab2">
+    <img slot="icon" src="">
+    tab2
+    </TabBar>
+    <TabBar id="tab3">
+    <img slot="icon" src="">
+    tab3
+    </TabBar>
+    <TabBar id="tab4">
+    <img slot="icon" src="">
+    tab4
+    </TabBar>
+    </Tabbar>
   </div>
 </template>
 
 <script>
 import HelloComponent from './components/hello'
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  import MintUI from 'mint-ui'
+  import 'mint-ui/lib/style.css'
+  import { Tabbar, TabItem,Button } from 'mint-ui'
+  import Vue from 'vue'
+
+  Vue.component(Tabbar.name, Tabbar);
+  Vue.component(TabItem.name, TabItem);
+  Vue.component(Button.name, Button);
+
+  export default {
+    data: function () {
+      return {
+        message: "Hello Vue!",
+        selected: "",
+      }
+    },
+    components: {
+      HelloComponent,
+      Button
     }
-  },
-  components: {
-    HelloComponent,
   }
-}
 </script>
 
 <style scoped>
